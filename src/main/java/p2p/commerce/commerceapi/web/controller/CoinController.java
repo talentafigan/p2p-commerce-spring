4252e8d462c018ecda4ad4d3b1585a0cbce81a46
Prasetya234 <putraprasetyadhany@gmail.com>
2023-07-03 13:38:33 +0700

    api price coin

---
+++ src/main/java/p2p/commerce/commerceapi/web/controller/CoinController.java
@@ -0,0 +1,21 @@
+package p2p.commerce.commerceapi.web.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import p2p.commerce.commerceapi.configuration.response.CommonResponse;
+import p2p.commerce.commerceapi.configuration.response.ResponseHelper;
+
+@RestController
+@RequestMapping("/api/coin")
+public class CoinController {
+
+    @Value("${app.coin-price}")
+    private int coinPrice;
+
+    @GetMapping
+    public CommonResponse<Integer> priceCoin() {
+        return ResponseHelper.ok(coinPrice);
+    }
+}
